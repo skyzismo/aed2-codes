5e1e4c0999531514968314494043978b2882acd1
skyzismo <lucas.petigato@hotmail.com>
2023-03-22 21:29:11 -0400

    primeiros arquivos

---
+++ README.md
@@ -0,0 +1,1 @@
+Repo para salvar códigos úteis da disciplina de AED II
---
+++ interpola.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    double notas_cc[] = {8.0, 8.5, 9.0};
+    double notas_ec[] = {8.0, 8.0, 8.0};
+    double notas_es[] = {7.5, 8.5, 9.5};
+
+    return 0;
+}
