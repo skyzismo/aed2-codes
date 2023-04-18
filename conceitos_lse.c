9e30372abd8779e66afc9d950a2c7f61d2324e20
skyzismo <lucas.petigato@hotmail.com>
2023-04-17 22:43:47 -0400

    add conceitos lse

---
+++ conceitos_lse.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/*
+
+typedef struct
+{
+    t_elemento_lse* inicio;
+    int tamanho;
+}t_lse; */
+
+typedef struct elem_lse{
+    int cargaUtil;
+    struct elem_lse* prox; //referência para o próximo endereço da lista
+}t_elemento_lse;
+
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
